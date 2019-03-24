a8198c168c141c6c393141539c3f32028adafda8
sun3stones <32724669+sun3stones@users.noreply.github.com>
2019-03-24 22:52:41 +0800

    add gateway&oauth2

--- mywechat/src/main/java/com/lei/mywechat/controller/WechatController.java
+++ mywechat/src/main/java/com/lei/mywechat/controller/WechatController.java
@@ -1,7 +1,6 @@
 package com.lei.mywechat.controller;
 
 import com.lei.mywechat.utils.CheckUtil;
-import com.netflix.discovery.util.StringUtil;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
